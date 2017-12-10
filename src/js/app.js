3f3c5f10e8e185067fc8e1ed3d214417255464ac
Yuri Artiukh <akella.a@gmail.com>
2017-12-10 12:31:59 +0200

    gogo

---
+++ src/js/app.js
@@ -0,0 +1,74 @@
+import {TimelineMax} from 'gsap';
+import 'pixi.js';
+
+
+let state = {pos:0};
+
+$('body').on('click',() => {
+  
+  let tl = new TimelineMax();
+  let tl1 = new TimelineMax();
+  tl
+    .to(state,2,{
+    	pos:1,
+    	onUpdate: function() {
+    		let perc = '-'+state.pos*100+'%';
+    		let perc1 = '-'+state.pos*state.pos*100+'%';
+    		let perc2 = '-'+state.pos*state.pos*state.pos*100+'%';
+    		// let perc = '-'+state.pos*100+'%';
+    		tl1
+    		.set('.slider__1 .slider__wrap',{x:perc},0)
+    		.set('.slider__2 .slider__wrap',{x:perc1},0)
+    		.set('.slider__3 .slider__wrap',{x:perc2},0);
+    		filter.uniforms.time = state.pos;
+    	}
+    });
+});
+
+
+// WEBGL implementation
+
+var app = new PIXI.Application(900,600);
+ 	document.body.appendChild(app.view);
+
+
+ 	// Create background image
+ 	var background = PIXI.Sprite.fromImage('img/5_.jpg');
+ 	var oldimage = PIXI.Sprite.fromImage('img/6_.jpg');
+ 	background.width = app.renderer.width;
+ 	background.height = app.renderer.height;
+
+
+ 
+ 	app.stage.addChild(background);
+
+
+ 	// Stop application wait for load to finish
+ 	app.stop();
+
+ 	PIXI.loader.add('shader', '/js/shader.frag')
+ 	    .load(onLoaded);
+
+ 	var filter;
+
+
+ 	function onLoaded(loader,res) {
+
+
+ 	    filter = new PIXI.Filter(null, res.shader.data);
+
+ 	    background.filters = [filter];
+
+ 	    filter.uniforms.currentimage = background._texture;
+ 	    filter.uniforms.oldimage = oldimage._texture;
+ 	    filter.uniforms.time = 0;
+ 	    app.start();
+ 	}
+
+
+ 	app.ticker.add(function(delta) {
+
+ 	});
+
+
+
---
+++ src/js/shader.frag
@@ -0,0 +1,45 @@
+
+precision mediump float;
+varying vec2 vTextureCoord;
+
+uniform float time;
+uniform sampler2D uSampler;
+uniform sampler2D currentimage;
+uniform sampler2D oldimage;
+
+
+
+void main(void) {
+	vec2 uv = vTextureCoord;
+	float mytime = time;
+	vec3 mask = vec3(0.0);
+	vec3 mask1 = vec3(0.0);
+	vec3 mask2 = vec3(0.0);
+	vec3 layer1,layer2,layer3;
+
+	mask += step(0.2,(0.2*uv.x+uv.y)*0.8);
+	mask1 += step(0.2,(0.2*uv.x+uv.y)*0.4);
+	mask2 = (1. - (1.0 - mask1)*mask);
+
+
+	vec4 currentimageT = texture2D(currentimage, vec2(uv.x - 1.0 + mytime,uv.y*1.5));
+	vec4 oldimageT = texture2D(oldimage, vec2(uv.x + mytime,uv.y*1.5));
+
+	layer1 = currentimageT.rgb + oldimageT.rgb;
+
+
+	vec4 currentimageT1 = texture2D(currentimage, vec2(uv.x - 1.0 + mytime*mytime*mytime,uv.y*1.5));
+	vec4 oldimageT1 = texture2D(oldimage, vec2(uv.x + mytime*mytime*mytime,uv.y*1.5));
+
+	layer2 = currentimageT1.rgb + oldimageT1.rgb;
+
+
+	vec4 currentimageT2 = texture2D(currentimage, vec2(uv.x - 1.0 + mytime*mytime,uv.y*1.5));
+	vec4 oldimageT2 = texture2D(oldimage, vec2(uv.x + mytime*mytime,uv.y*1.5));
+
+	layer3 = currentimageT2.rgb + oldimageT2.rgb;
+
+	vec3 all =  (1.-mask)*layer2 + mask1*layer1 + (1.0 - mask2)*layer3;
+	gl_FragColor = vec4(all,0.0);
+}
+
